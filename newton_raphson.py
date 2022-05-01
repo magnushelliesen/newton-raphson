179f82d7c19af53f64e87c7460457eae879c008c
magnushelliesen <104299371+magnushelliesen@users.noreply.github.com>
2022-05-01 22:36:51 +0200

    Update newton_raphson.py

--- newton_raphson.py
+++ newton_raphson.py
@@ -1,14 +1,8 @@
-#####################################
-# Written by Magnus Kvåle Helliesen #
-#####################################
-
-import numpy as np
-
 def newton_raphson(f, init, **kwargs):
 	"""
 	TBA
 	"""
-	
+
 	if 'args' in kwargs:
 		args = kwargs['args']
 	else:
@@ -16,7 +10,7 @@ def newton_raphson(f, init, **kwargs):
 	if 'jac' in kwargs:
 		jac = kwargs['jac']
 	else:
-		print('ERROR: Newton-Raphson requires symbolic Jacobian matrix')
+		print('ERROR: Newton-Raphson require symbolic Jacobian matrix')
 		return {'x': np.array(init), 'fun': np.array(f(init, *args)), 'success': False}
 	if 'tol' in kwargs:
 		tol = kwargs['tol']
@@ -26,17 +20,16 @@ def newton_raphson(f, init, **kwargs):
 		maxiter = kwargs['maxiter']
 	else:
 		maxiter = 10
-	
+
 	success = True
 	x_i = np.array(init)
-	f_i = np.array(f(x_i, *args))
+	f_i = np.array(f(init, *args))
 	i = 0
 	while np.max(np.abs(f_i)) > tol:
-		J_i = np.array(jac(x_i.tolist(), *args))
 		if i == maxiter:
 			success = False
 			break
-		x_i = x_i-np.matmul(np.linalg.inv(J_i), f_i)
+		x_i = x_i-np.matmul(np.linalg.inv(np.array(jac(x_i.tolist(), *args))), f_i)
 		f_i = np.array(f(x_i.tolist(), *args))
 		i+=1
 	return {'x': x_i, 'fun': f_i, 'success': success}
