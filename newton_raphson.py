e805e4ae288f203463e26bd65cd502b3ed437c39
Magnus Helliesen <104299371+magnushelliesen@users.noreply.github.com>
2023-03-20 09:00:43 +0100

    Lot's of stuff
    Error in stop requirement fixed. Other stuff too.

--- newton_raphson.py
+++ newton_raphson.py
@@ -1,48 +1,32 @@
 import numpy as np
 
 
-def newton_raphson(f, jac, init, **kwargs):
-	"""
-	Function that takes a function f and a jacobi matrix jac and finds a root
-	"""
-
-	# Check for any kwargs
-	if 'args' in kwargs.keys():
-		args = kwargs.get('args')
-	else:
-		args = ()
-	if 'tol' in kwargs.keys():
-		tol = kwargs.get('tol')
-	else:
-		tol = 1e-10
-	if 'maxiter' in kwargs.keys():
-		maxiter = kwargs.get('maxiter')
-	else:
-		maxiter = 10
-
-	# Initialize loop
-	x_i = init
-	i = 1
-	while True:
-		# If maxiter reached, then break with failure
-		if i > maxiter:
-			success = False
-			break
-
-		# Calculate functional value at x_i
-		f_i = np.array(f(x_i.tolist(), *args))
-
-		# If function closer to zero than tolerance, then break with success
-		if np.max(np.abs(f_i)) <= tol:
-			success = True
-			break
-
-		# Try to calculate the next iteration, if exception is thrown break with failure
-		try:
-			x_i = x_i-np.matmul(np.linalg.inv(np.array(jac(x_i.tolist(), *args))), f_i)
-			i += 1
-		except np.linalg.LinAlgError:
-			success = False
-			break
+def _newton_raphson(f, init, args=None, jac=None, tol=10**-7, maxiter=10):
+	if args is None:
+		args = tuple([])
+	if jac is None:
+		raise ValueError('Symbolic Jacobian must be specified')
 	
-	return {'x': x_i, 'f': f_i, 'success': success, 'iter': i}
+	success = True
+	status = 0
+	x_i = init
+	f_i = np.array(f(init.tolist(), *args))
+	i = 0
+	while np.max(np.abs(f_i)) > 0:
+	    if i == maxiter:
+		success = False
+		status = 1
+		break
+	    try:
+		x_i_new = x_i-np.matmul(np.linalg.inv(np.array(jac(x_i.tolist(), *args))), f_i)
+		if np.max(np.abs(x_i_new-x_i)) <= tol:
+		    break
+		x_i = x_i_new
+	    except np.linalg.LinAlgError:
+		success = False
+		status = 2
+		break
+	    f_i = np.array(f(x_i, *args))
+	    i += 1
+
+	return {'x': x_i, 'fun': f_i, 'success': success, 'status': status}
